4c7f5da1d6b1123898feb23f80451d13cbad3ae8
Matt Osborne <MattJOzzy@gmail.com>
2023-03-31 00:54:25 -0500

    worked on resume and contact components

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,58 @@
 import React from 'react'
 import './contact.css'
+import { AiOutlineMail } from 'react-icons/ai'
+import { AiOutlineLinkedin } from 'react-icons/ai'
+import { AiOutlinePhone } from 'react-icons/ai'
+// import { useRef } from 'react'
+// import emailjs from '@emailjs-com'
 
 const Contact = () => {
+  // const form = useRef();
+  // const sendEmail = (e) => {
+  //   e.preventDefault();
+
+  //   emailjs.sendForm('service_plc16g7', 'template_m8emsxk', form.current, '0fTH_K6hHYolqfvaG')
+  //     .then((result) => {
+  //         console.log(result.text);
+  //     }, (error) => {
+  //         console.log(error.text);
+  //     });
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className='container contact_container'>
+        <div className='contact_options'>
+          <article className='contact_option'>
+            <AiOutlineMail className='contact_option-icon' />
+            <h4>Email</h4>
+            <h5>MattJOzzy@gmail.com</h5>
+            <a href='mailto:mattjozzy@gmail.com' target='_blank'>Send a message</a>
+          </article>
+          <article className='contact_option'>
+            <AiOutlineLinkedin className='contact_option-icon' />
+            <h4>LinkedIn</h4>
+            <h5>mattjosborne96</h5>
+            <a href='https://www.linkedin.com/in/mattjosborne96/' target='_blank'>Profile</a>
+          </article>
+          <article className='contact_option'>
+            <AiOutlinePhone className='contact_option-icon' />
+            <h4>Phone</h4>
+            <h5>312-468-4711</h5>
+            <a href='https://api.whatsapp.com/send?phone=13124684711' target='_blank'>Call me</a>
+          </article>
+        </div>
+
+        {/* <form ref={form} onSubmit={sendEmail}>
+          <input type='text' name='name' placeholder='Your Name' required />
+          <input type='email' name='email' placeholder='Your Email' required />
+          <textarea name='message' rows='7' placeholder='Your Message' required />
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form> */}
+
+      </div>
+    </section>
   )
 }
 
--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './resume.css'
+import { AiOutlineCheckCircle } from 'react-icons/ai'
 
 const Resume = () => {
   return (
@@ -11,11 +12,78 @@ const Resume = () => {
       <div className='container experience_container'>
 
         <div className='experience_frontend'>
-
+          <h3> Frontend Development</h3>
+          <div className='experience_content'>
+            <article className='experience_details'>
+            <AiOutlineCheckCircle className='experience_details-icon'/>
+              <div>
+                <h4>HTML</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience_details'>
+            <AiOutlineCheckCircle className='experience_details-icon'/>
+              <div>
+                <h4>CSS</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience_details'>
+            <AiOutlineCheckCircle className='experience_details-icon'/>
+              <div>
+                <h4>JavaScript</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience_details'>
+            <AiOutlineCheckCircle className='experience_details-icon'/>
+              <div>
+                <h4>Bootstrap</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience_details'>
+            <AiOutlineCheckCircle className='experience_details-icon'/>
+              <div>
+                <h4>React</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+          </div>
         </div>
 
-        <div className='experience_backend'>
-
+        <div className='experience_frontend'>
+          <h3> Backend Development</h3>
+          <div className='experience_content'>
+            <article className='experience_details'>
+            <AiOutlineCheckCircle className='experience_details-icon'/>
+              <div>
+                <h4>Node JS</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience_details'>
+              <AiOutlineCheckCircle className='experience_details-icon'/>
+              <div>
+                <h4>MongoDB</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience_details'>
+            <AiOutlineCheckCircle className='experience_details-icon'/>
+              <div>
+                <h4>MySQL</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience_details'>
+            <AiOutlineCheckCircle className='experience_details-icon'/>
+              <div>
+                <h4>NoSQL</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+          </div>
         </div>
 
       </div>
