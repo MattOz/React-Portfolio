8f96df4d1f40ae8974a5ceef248b75d6b1dac706
Matt Osborne <MattJOzzy@gmail.com>
2023-03-30 23:11:58 -0500

    worked on components and styling

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,53 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/testImage.jpg'
+import { FaAward } from 'react-icons/fa'
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>About</h5>
+      <h2>Get To Know Me</h2>
+
+      <div className='container about_container'>
+        
+        {/* <div className='about_me'>
+          <div className='about_me-image'>
+            <img src={ME} alt='about image' />
+          </div>
+        </div> */}
+      
+
+      <div className='about_content'>
+        <div className='about_cards'>
+          <article className='about_card'>
+            <FaAward className='about_icon' />
+            <h5>Title 1</h5>
+            <small>Lorem ipsum dolor sit amet consectetur adipisicing elit.</small>
+          </article>
+          <article className='about_card'>
+            <FaAward className='about_icon' />
+            <h5>Title 2</h5>
+            <small>Lorem ipsum dolor sit amet consectetur adipisicing elit.</small>
+          </article>
+          <article className='about_card'>
+            <FaAward className='about_icon' />
+            <h5>Title 3</h5>
+            <small>Lorem ipsum dolor sit amet consectetur adipisicing elit.</small>
+          </article>
+        </div>
+        
+ 
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor numquam sapiente maiores aliquid voluptates deserunt non accusantium quia optio excepturi! Soluta ullam libero impedit tempore ex suscipit, possimus minus odit?</p>
+        <div className='contact_button'>
+        <a href='#contact' className='btn btn-primary'>Let's Talk</a>
+        </div>
+
+      </div>
+
+      </div>
+
+    </section>
   )
 }
 
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,62 @@
 import React from 'react'
 import './portfolio.css'
 
+const data = [
+  {
+    id: 1,
+    title: 'Project 1',
+    github: 'https://github.com',
+    demo: 'https://github.com'
+  },
+  {
+    id: 2,
+    title: 'Project 2',
+    github: 'https://github.com',
+    demo: 'https://github.com'
+  },
+  {
+    id: 3,
+    title: 'Project 3',
+    github: 'https://github.com',
+    demo: 'https://github.com'
+  },
+  {
+    id: 4,
+    title: 'Project 4',
+    github: 'https://github.com',
+    demo: 'https://github.com'
+  },
+  {
+    id: 5,
+    title: 'Project 5',
+    github: 'https://github.com',
+    demo: 'https://github.com'
+  },
+  {
+    id: 6,
+    title: 'Project 6',
+    github: 'https://github.com',
+    demo: 'https://github.com'
+  },
+]
+
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className='container portfolio_container'>
+
+        <article className='portfolio_item'>
+          <div className="portfolio_item-image"></div>
+          <h3>Project 1</h3>
+          <a href="https://github.com" className='btn' target='_blank'>Github</a>
+          <a href="https://github.com" className='btn btn-primary' target='_blank'>Live Link</a>
+        </article>
+
+      </div>
+    </section>
   )
 }
 
