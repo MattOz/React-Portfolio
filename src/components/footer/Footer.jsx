d4b7f141f13b9d2c5d3ed2e76801349c12dfa7e3
Matt Osborne <MattJOzzy@gmail.com>
2023-04-01 16:15:17 -0500

    finished footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './footer.css'
+import {AiFillGithub} from 'react-icons/ai'
+import {AiFillLinkedin} from 'react-icons/ai'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+
+      <ul className='permalinks'>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#about'>About</a></li>
+        <li><a href='#portfolio'>Portfolio</a></li>
+        <li><a href='#resume'>Resume</a></li>
+        <li><a href='#contact'>Contact</a></li>
+      </ul>
+
+      <div className='footer_socials'>
+        <a href="https://github.com/MattOz"><AiFillGithub/></a>
+        <a href="https://github.com/MattOz"><AiFillLinkedin/></a>
+      </div>
+      
+    </footer>
   )
 }
 
